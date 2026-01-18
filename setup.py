be5af56ef06e13671f90e1b2a164ef25df103538
Aditya K <notaditya2k4@gmail.com>
2026-01-19 01:58:43 +0530

    requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Aditya Amod Kumbhar",
+    author_email="notaditya2k4@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
